ea805708b294c1bcf8a844954fb8b7c70433ed5c
Fawaz441 <abdulsalamfawaz4@gmail.com>
2021-07-08 00:41:14 +0100

    list collapsing'

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var arrow = document.querySelector("#arrow > img")
 var texts = document.querySelectorAll('.items span')
-var main = document.querySelector('.main')
+var anything = document.querySelector('.main')
+
 
 arrow.addEventListener('click',function(){
     // on clicking the arrow, toggle the class .hide on texts
-    main.classList.toggle('small')
+    anything.classList.toggle('small')
     for(var i=0;i<texts.length;i++){
         texts[i].classList.toggle('hide')
     }
+})
+
+
+// list collapse
+var list = document.getElementById('content')
+var listToggler = document.getElementById('list_toggler')
+
+listToggler.addEventListener('click',function(){
+    list.classList.toggle('hide')
 })
 
